66d4b0499c4060b7280ed2c4f42880e2fc2ea9fb
Rifa9oct <kohinuraktherr@gmail.com>
2025-01-06 09:17:59 +0600

    correct the responsiveness

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-scroll";
 import logo from "../assets/logo.png"
 import "./navbar.css"
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
 
@@ -23,9 +24,10 @@ const Navbar = () => {
                         </Link>
                     </div>
                     <div className="block lg:hidden dropdown dropdown-end">
-                        <label tabIndex={0} className="btn-sm btn-outline lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        <label tabIndex={0} className="lg:hidden">
+                            <GiHamburgerMenu className="text-white text-xl" />
                         </label>
+                        
                         <ul tabIndex={0} className="rounded-lg flex text-base text-white flex-col gap-4 dropdown-content mt-3 z-[1] p-6 py-6 shadow shadow-gray-500 bg-[#271a2b] w-52">
                             {navLinks}
                         </ul>
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -5,40 +5,36 @@ import ssc from "../../assets/ssc.png"
 const Education = () => {
     return (
         <div className="max-w-[1280px] text-white mx-auto mt-[160px] md:mt-10 lg:mt-[120px] mb-[150px]" id="education">
-            < h1 className="text-5xl font-extrabold text-center font-serif" >Education</h1 >
+            < h1 className="text-4xl md:text-5xl font-extrabold text-center font-serif" >Education</h1 >
 
             <div className='flex flex-col lg:flex-row gap-[100px] lg:gap-8 justify-center mt-[130px]'>
-                <div className='relative'>
-                    <div className="absolute -top-[60px] left-[165px] md:left-[335px] lg:left-[138px] flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto border-2 border-red-600 bg-blue-500">
+                <div className="relative w-[350px] md:w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
+                    <h3 className="mt-3 text-sm md:text-base">Bsc (Hons) (2021- present)</h3>
+                    <h4 className="text-lg md:text-xl font-bold">Computer Science and Engineering</h4>
+                    <h3 className="text-sm md:text-base">Tejgaon College, Dhaka, Bangladesh</h3>
+
+                    <div className="absolute -top-[50px] left-[130px] md:left-[145px] flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto border-2 border-red-600 bg-blue-500">
                         <img src={bsc} alt="bsc_hons" />
                     </div>
-
-                    <div className="w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
-                        <h3>Bsc (Hons) (2021- present)</h3>
-                        <h4 className="text-xl font-bold">Computer Science and Engineering</h4>
-                        <h3>Tejgaon College, Dhaka, Bangladesh</h3>
-                    </div>
                 </div>
-                <div className='relative'>
-                    <div className="absolute -top-[60px] left-[165px] md:left-[335px] lg:left-[138px] flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto">
+
+                <div className="relative w-[350px] md:w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
+                    <h3 className="mt-3 text-sm md:text-base">HSC (2018 - 2019)</h3>
+                    <h4 className="text-lg md:text-xl font-bold">Higher Secondary Certificate</h4>
+                    <h3 className="text-sm md:text-base">Police Lines School and College, Rangpur</h3>
+
+                    <div className="absolute -top-[50px] left-[130px] md:left-[145px] flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto">
                         <img src={hsc} alt="hsc" />
                     </div>
-
-                    <div className="w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
-                        <h3>HSC (2018 - 2019)</h3>
-                        <h4 className="text-xl font-bold">Higher Secondary Certificate</h4>
-                        <h3>Police Lines School and College, Rangpur</h3>
-                    </div>
                 </div>
-                <div className='relative'>
-                    <div className="absolute -top-[60px] left-[165px] md:left-[335px] lg:left-[138px] flex justify-center items-center w-[100px] h-[100px] rounded-full border-2 border-red-700 bg-[#01D201]">
-                        <img src={ssc} alt="ssc" className='w-[80px]' />
-                    </div>
 
-                    <div className="w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
-                        <h3>SSC (2016 - 2017)</h3>
-                        <h4 className="text-xl font-bold">Secondary School Certificate</h4>
-                        <h3>Govt Girls High School, Rangpur</h3>
+                <div className="relative w-[350px] md:w-[380px] mx-auto pt-[50px] border border-purple-400 shadow-inner shadow-gray-200 bg-[#9020aa] rounded-[20px] p-8 text-center">
+                    <h3 className="mt-3 text-sm md:text-base">SSC (2016 - 2017)</h3>
+                    <h4 className="text-lg md:text-xl font-bold">Secondary School Certificate</h4>
+                    <h3 className="text-sm md:text-base">Govt Girls High School, Rangpur</h3>
+
+                    <div className="absolute -top-[50px] left-[130px] md:left-[145px] flex justify-center items-center w-[100px] h-[100px] rounded-full border-2 border-red-700 bg-[#01D201]">
+                        <img src={ssc} alt="ssc" className='w-[80px]' />
                     </div>
                 </div>
             </div>
